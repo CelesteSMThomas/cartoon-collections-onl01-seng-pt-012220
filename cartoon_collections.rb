72795f76ed2ec8ccd64756cba984dca2dcfd2573
Celeste Thomas <celestesmthomas@gmail.com>
2020-02-05 00:15:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |names, index|
+    puts "#{index+1} #{names}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |array|
+     new_array << array.capitalize += "!"
+   end
 end
 
 def long_planeteer_calls# code an argument here
